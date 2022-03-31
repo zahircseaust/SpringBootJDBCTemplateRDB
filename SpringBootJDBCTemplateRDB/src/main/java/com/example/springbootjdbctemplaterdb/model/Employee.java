ccf8b8b73bd546ac334cf00efab319a13d7d0911
admin <admin@ApsD-Zahirul-L>
2022-03-31 23:11:19 +0600

    first commit

--- SpringBootJDBCTemplateRDB/src/main/java/com/example/springbootjdbctemplaterdb/model/Employee.java
+++ SpringBootJDBCTemplateRDB/src/main/java/com/example/springbootjdbctemplaterdb/model/Employee.java
@@ -1,42 +1,53 @@
 package com.example.springbootjdbctemplaterdb.model;
 
 public class Employee {
-	
-	public Employee() {}
-     
-	 public Employee(Long id, String name, String location, String department) {
+
+	private Long id;
+	private String name;
+	private String location;
+	private String department;
+
+	public Employee() {
+	}
+
+	public Employee(Long id, String name, String location, String department) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.location = location;
 		this.department = department;
 	}
-	private Long id;
-	 public Long getId() {
+
+	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
-	private String name;
-	 private String location;
-	 private String department;
+
 }
